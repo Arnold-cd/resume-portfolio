2a3b312e08abe866683e4b0f6ffa0a933350de7c
Arnold-cd <2013arnold@gmail.com>
2025-01-22 22:43:58 +0530

    Updated portfolio content

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     {
       id: 2,
       title: "EXPERIENCE",
-      description: "Depression (2024 - Present)\nApp developer at Nibiaa Devices (Jan-Dec, 2023)",
+      description: "Depression 🤓🔥🤣 (2024 - Present)\nApp developer at Nibiaa Devices (Jan-Dec, 2023)",
       image: experienceImage,
       color: "rgb(28, 54, 152)"
     },
